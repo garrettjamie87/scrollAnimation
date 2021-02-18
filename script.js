cd6c62881da51bd4b08dbb4b6e72aba9337d5e1d
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-18 18:48:45 +0100

    alter color fix bugs and final commit

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const boxes = document.querySelectorAll('.box');
 window.addEventListener('scroll', checkBoxes)
 
 function checkBoxes() {
-     const triggerBottom = window.innerHeight / 5*4
+     const triggerBottom = window.innerHeight / 5 * 4
 
-     boxes.forEach((box)=>{
+     boxes.forEach(box =>{
            const boxTop = box.getBoundingClientRect().top
 
            if(boxTop < triggerBottom) {
