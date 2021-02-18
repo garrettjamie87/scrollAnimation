d16306753fd3fe8ce2c404ba30f2b94d79c15e05
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-18 18:28:34 +0100

    add ifElse statement to add or remove .show class

--- script.js
+++ script.js
@@ -7,5 +7,11 @@ function checkBoxes() {
 
      boxes.forEach((box)=>{
            const boxTop = box.getBoundingClientRect().top
+
+           if(boxTop < triggerBottom) {
+                 box.classList.add('show')
+           }else{
+                 box.classList.remove('show')
+           }
      })
 }
